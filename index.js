e77bcf2b17e3aa92e9d895e0dab6c376e750e09a
Harshitsgit <harshit.g@shaligraminfotech.com>
2024-12-21 12:02:12 +0530

    Initai code added

---
+++ index.js
@@ -0,0 +1,6 @@
+const name = "Harshit";
+const age = 23;
+
+function giveInfo() {
+  return `name ${name} , age ${age}`;
+}
---
+++ script.js
@@ -0,0 +1,8 @@
+const name = "Harshit";
+const age = 23;
+
+function giveInfo() {
+  return `name ${name} , age ${age}`;
+}
+
+const demo = "";
